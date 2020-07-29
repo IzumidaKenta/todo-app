d9f7b37db0fdafc2ef45a1ce1673530f1b032c62
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-07-29 19:38:33 +0900

    POST実装完了,PUT実装途中

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "io/ioutil"
     "log"
     "net/http"
-    "strconv"
+
+    "./utils"
 
     "github.com/gorilla/mux"
-    "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
@@ -27,65 +28,100 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func returnAllTodos(w http.ResponseWriter, r *http.Request) {
     // Code for returning all items here
 
-    // DB接続する(user、passwordは適宜修正)
-    db, _ := gorm.Open("mysql", "root:Cbeg1nner@/todos?charset=utf8&parseTime=True&loc=Local")
+    // DB接続する
+    db := utils.GetConnection()
     defer db.Close()
-    // ロガーを有効にすると、詳細なログを表示します
-    db.LogMode(true)
 
     // 空のスライス
     var todoList []Todo
     // SELECT文が発行されて結果がtodoListに入る
     db.Find(&todoList)
 
-    respondWithJson(w, http.StatusOK, todoList)
+    utils.RespondWithJSON(w, http.StatusOK, todoList)
 }
 
 // Controller for the /items/{id} route
 func returnSingleTodo(w http.ResponseWriter, r *http.Request) {
-    // Code for returning a single item here
-    // Get query parameters using Mux
-    vars := mux.Vars(r)
-
-    // Convert {todoid} parameter from string to int
-    key, err := strconv.Atoi(vars["id"])
-
+    key, err := utils.GetID(r)
     // If {todoid} parameter is not valid int
     if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
         return
     }
 
     // DB接続
-    db, _ := gorm.Open("mysql", "root:Cbeg1nner@/todos?charset=utf8&parseTime=True&loc=Local")
+    db := utils.GetConnection()
     defer db.Close()
-    db.LogMode(true)
+
 
     var todo Todo
     // IDで検索しに行く
     db.Where("id = ?", key).Find(&todo)
 
-    respondWithJson(w, http.StatusOK, todo)
+    utils.RespondWithJSON(w, http.StatusOK, todo)
 }
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+    //リクエストボディ取得
+    body, err := ioutil.ReadAll(r.Body)
+    defer r.Body.Close()
+    if err != nil {
+      utils.RespondWithError(w, http.StatusBadRequest, "Invalid request")
+      return
+    }
+
+    var todo Todo
+    //読み込んだJSONを構造体に変換
+    if err := json.Unmarshal(body, &todo); err != nil {
+      utils.RespondWithError(w, http.StatusBadRequest, "JSON Unmarshaling failed .")
+      return
+    }
+
+    //DB接続
+    db := utils.GetConnection()
+    defer db.Close()
+
+    //DBにINSERTする
+    db.Create(&todo)
+
+    utils.RespondWithJSON(w, http.StatusOK, todo)
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+    key, err := utils.GetID(r)
+    // If {todoid} parameter is not valid int
+    if err != nil {
+        utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+
+    var todo Todo
+    var checked bool
+
+    // DB接続
+    db := utils.GetConnection()
+    defer db.Close()
+
+    //現在のcheckedの値を読み取る
+    db.Model(&checked).Select("checked").Where("id = ?", key)
+    print(checked)
+
+    // Update実行
+    db.Model(&todo).Select(key).Update("checked",!checked)
+
+    utils.RespondWithJSON(w, http.StatusOK, todo)
+}
+
 func handleRequests() {
     myRouter := mux.NewRouter().StrictSlash(true)
     myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/api/v1/todos", returnAllTodos)
-    myRouter.HandleFunc("/api/v1/todos/{id}", returnSingleTodo)
-    log.Fatal(http.ListenAndServe(":8000", myRouter))
+    myRouter.HandleFunc("/api/v1/todos", returnAllTodos).Methods("GET")
+    myRouter.HandleFunc("/api/v1/todos/{id}", returnSingleTodo).Methods("GET")
+    myRouter.HandleFunc("/api/v1/todos", createTodo).Methods("POST")
+    myRouter.HandleFunc("/api/v1/todos/{id}", updateTodo).Methods("PUT")
+    log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
     handleRequests()
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-    respondWithJson(w, code, map[string]string{"error": msg})
-}
-
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-    response, _ := json.Marshal(payload)
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(code)
-    w.Write(response)
 }
